523ffc1a85f40469beaf73d69dbbd8341b4f2a96
nemanjam <nemanja.mitic.elfak@hotmail.com>
2019-10-16 18:59:35 +0200

    add apollo middleware and afterware to attach auth header

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { AsyncStorage } from 'react-native';
 import { Root } from 'native-base';
 
 import { createAppContainer } from 'react-navigation';
@@ -12,6 +11,8 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { WebSocketLink } from 'apollo-link-ws';
 import { getMainDefinition } from 'apollo-utilities';
+import { setContext } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
 
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -19,6 +20,7 @@ import { persistStore, persistCombineReducers } from 'redux-persist';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { PersistGate } from 'redux-persist/lib/integration/react';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -28,6 +30,8 @@ import UserProfileScreen from './screens/UserProfileScreen';
 import GroupDetailsScreen from './screens/GroupDetailsScreen';
 
 import authReducer from './store/reducers/authReducer';
+
+// ============== navigation ================
 
 const AppNavigator = createStackNavigator(
   {
@@ -46,31 +50,7 @@ const AppNavigator = createStackNavigator(
 
 const AppContainer = createAppContainer(AppNavigator);
 
-const uri = '10.0.2.2:5000';
-// const uri = '192.168.0.185:5000';
-const httpLink = createHttpLink({ uri: `http://${uri}` });
-
-const wsLink = new WebSocketLink({
-  uri: `ws://${uri}/graphql`,
-  options: {
-    reconnect: true,
-  },
-});
-
-const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
-  },
-  wsLink,
-  httpLink,
-);
-
-export const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache(),
-});
-
+// ============ redux ===============
 const config = {
   key: 'root',
   storage: AsyncStorage,
@@ -87,6 +67,81 @@ const store = createStore(
 );
 
 const persistor = persistStore(store);
+
+// =================================
+// =========== apollo ==============
+const uri = '10.0.2.2:5000';
+// const uri = '192.168.0.185:5000';
+const httpLink = createHttpLink({ uri: `http://${uri}` });
+
+const wsLink = new WebSocketLink({
+  uri: `ws://${uri}/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+
+// middleware for requests
+const middlewareLink = setContext((req, previousContext) => {
+  const { jwt } = store.getState().authReducer.user;
+  if (jwt) {
+    return {
+      headers: {
+        authorization: `Bearer ${jwt}`,
+      },
+    };
+  }
+  return previousContext;
+});
+
+// afterware for responses
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  let shouldLogout = false;
+  if (graphQLErrors) {
+    console.log({ graphQLErrors });
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.log({ message, locations, path });
+      if (message === 'Unauthorized') {
+        shouldLogout = true;
+      }
+    });
+
+    if (shouldLogout) {
+      store.dispatch(logout());
+    }
+  }
+  if (networkError) {
+    console.log('[Network error]:');
+    console.log({ networkError });
+    if (networkError.statusCode === 401) {
+      store.dispatch(logout());
+    }
+  }
+});
+
+const requestLink = ({ httpLink, wsLink }) => {
+  return split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query);
+      return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    wsLink,
+    httpLink,
+  );
+};
+
+const link = ApolloLink.from([
+  errorLink,
+  requestLink({
+    httpLink: middlewareLink.concat(httpLink),
+    wsLink: wsLink,
+  }),
+]);
+
+export const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 const App = props => {
   return (
