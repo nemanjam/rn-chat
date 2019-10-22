dadac99996e617ba77e51c5439b7c0dd2667fb05
nemanjam <nemanja.mitic.elfak@hotmail.com>
2019-10-22 16:14:43 +0200

    add new seed

--- server/src/data/seed.js
+++ server/src/data/seed.js
@@ -3,200 +3,57 @@ import { UserModel, ChatModel, MessageModel, GroupModel } from './connectors';
 import bcrypt from 'bcrypt';
 
 const seed = async () => {
-  const hash = await bcrypt.hash('123456', 10);
-  return Promise.all([
-    UserModel.create({
-      username: 'firstUser',
-      email: 'email1@email.com',
+  try {
+    const hash = await bcrypt.hash('123456', 10);
+    const user1 = await UserModel.create({
+      username: `firstUser`,
+      email: `email@email.com`,
       avatar: faker.internet.avatar(),
       description: faker.lorem.sentences(3),
       password: hash,
-      isActive: true,
       lastActiveAt: new Date(),
-    }),
-    UserModel.create({
-      username: 'secondUser',
-      email: 'email2@email.com',
-      avatar: faker.internet.avatar(),
-      description: faker.lorem.sentences(3),
-      password: hash,
-      isActive: true,
-      lastActiveAt: new Date(),
-    }),
-    UserModel.create({
-      username: 'thirdUser',
-      email: 'email3@email.com',
-      avatar: faker.internet.avatar(),
-      description: faker.lorem.sentences(3),
-      password: hash,
-      isActive: true,
-      lastActiveAt: new Date(),
-    }),
-    UserModel.create({
-      username: 'fourthUser',
-      email: 'email4@email.com',
-      avatar: faker.internet.avatar(),
-      description: faker.lorem.sentences(3),
-      password: hash,
-      isActive: true,
-      lastActiveAt: new Date(),
-    }),
-    UserModel.create({
-      username: 'fifthUser',
-      email: 'email5@email.com',
-      avatar: faker.internet.avatar(),
-      description: faker.lorem.sentences(3),
-      password: hash,
-      isActive: true,
-      lastActiveAt: new Date(),
-    }),
-    ChatModel.create({
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }),
-    ChatModel.create({
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }),
-    ChatModel.create({
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }),
-    ChatModel.create({
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }),
-    MessageModel.create({
-      text: faker.lorem.sentences(3),
-      createdAt: new Date(),
-    }),
-    MessageModel.create({
-      text: faker.lorem.sentences(3),
-      createdAt: new Date(),
-    }),
-    MessageModel.create({
-      text: faker.lorem.sentences(3),
-      createdAt: new Date(),
-    }),
-    MessageModel.create({
-      text: faker.lorem.sentences(3),
-      createdAt: new Date(),
-    }),
-    MessageModel.create({
-      text: faker.lorem.sentences(3),
-      createdAt: new Date(),
-    }),
-    MessageModel.create({
-      text: faker.lorem.sentences(3),
-      createdAt: new Date(),
-    }),
-    MessageModel.create({
-      text: faker.lorem.sentences(3),
-      createdAt: new Date(),
-    }),
-    MessageModel.create({
-      text: faker.lorem.sentences(3),
-      createdAt: new Date(),
-    }),
-    GroupModel.create({
-      name: 'firstGroup',
-      avatar: faker.internet.avatar(),
-      description: faker.lorem.sentences(3),
-      isPrivate: true,
-    }),
-    GroupModel.create({
-      name: 'secondGroup',
-      avatar: faker.internet.avatar(),
-      description: faker.lorem.sentences(3),
-      isPrivate: false,
-    }),
-  ])
-    .then(
-      ([
-        firstUser,
-        secondUser,
-        thirdUser,
-        fourthUser,
-        fifthUser,
-        firstChat,
-        secondChat,
-        thirdChat,
-        fourthChat,
-        firstMessage,
-        secondMessage,
-        thirdMessage,
-        fourthMessage,
-        fifthMessage,
-        sixthMessage,
-        seventhMessage,
-        eighthMessage,
-        firstGroup,
-        secondGroup,
-      ]) => {
-        return Promise.all([
-          firstUser.addChat(firstChat),
-          secondUser.addChat(firstChat),
-          firstUser.addFriend(secondUser),
-          secondUser.addFriend(firstUser), //
-          firstMessage.setChat(firstChat),
-          secondMessage.setChat(firstChat),
-          firstUser.setMessage(secondMessage),
-          secondUser.setMessage(firstMessage),
+    });
+    [...Array(5).keys()].map(async (index, i) => {
+      const user2 = await UserModel.create({
+        username: `user${index}`,
+        email: `email${index}@email.com`,
+        avatar: faker.internet.avatar(),
+        description: faker.lorem.sentences(3),
+        password: hash,
+        lastActiveAt: new Date(),
+      });
+      const chat1 = await ChatModel.create({});
+      const message1 = await MessageModel.create({
+        text: faker.lorem.sentences(3),
+      });
+      const message2 = await MessageModel.create({
+        text: faker.lorem.sentences(3),
+      });
+      const group1 = await GroupModel.create({
+        name: `group${index}`,
+        avatar: faker.internet.avatar(),
+        description: faker.lorem.sentences(3),
+        isPrivate: !!(index % 2), // 0,1
+      });
 
-          firstUser.addChat(secondChat),
-          thirdUser.addChat(secondChat),
-          firstUser.addFriend(thirdUser),
-          thirdUser.addFriend(firstUser), //
-          thirdMessage.setChat(secondChat),
-          fourthMessage.setChat(secondChat),
-          firstUser.setMessage(fourthMessage),
-          thirdUser.setMessage(thirdMessage),
+      await group1.addUser(user1);
+      await group1.addUser(user2);
+      await group1.setOwner(user1);
+      await message1.setChat(chat1);
+      await message2.setChat(chat1);
+      await message1.setUser(user1);
+      await message2.setUser(user2);
+      await chat1.setGroup(group1);
+      await user1.addChat(chat1);
+      await user2.addChat(chat1);
 
-          /*
-          firstUser.addChat(thirdChat),
-          fourthUser.addChat(thirdChat),
-          // firstUser.addFriend(fourthUser),
-          // fourthUser.addFriend(firstUser),
-          fifthMessage.setChat(thirdChat),
-          sixthMessage.setChat(thirdChat),
-          firstUser.setMessage(sixthMessage),
-          fourthUser.setMessage(fifthMessage),
-
-          firstUser.addChat(fourthChat),
-          fifthUser.addChat(fourthChat),
-          // firstUser.addFriend(fifthUser),
-          // fifthUser.addFriend(firstUser),
-          seventhMessage.setChat(fourthChat),
-          eighthMessage.setChat(fourthChat),
-          firstUser.setMessage(eighthMessage),
-          fifthUser.setMessage(seventhMessage),
-          */
-
-          firstUser.addGroup(firstGroup),
-          fourthUser.addGroup(firstGroup),
-          fifthMessage.setChat(thirdChat),
-          sixthMessage.setChat(thirdChat),
-          thirdChat.setGroup(firstGroup),
-          thirdChat.addUser(firstUser),
-          thirdChat.addUser(fourthUser),
-          firstUser.setMessage(sixthMessage),
-          fourthUser.setMessage(fifthMessage),
-          firstGroup.setOwner(firstUser),
-
-          firstUser.addGroup(secondGroup),
-          fifthUser.addGroup(secondGroup),
-          seventhMessage.setChat(fourthChat),
-          eighthMessage.setChat(fourthChat),
-          fourthChat.setGroup(secondGroup),
-          fourthChat.addUser(firstUser),
-          fourthChat.addUser(fifthUser),
-          firstUser.setMessage(eighthMessage),
-          fifthUser.setMessage(seventhMessage),
-          secondGroup.setOwner(firstUser),
-        ]);
-      },
-    )
-    .catch(error => console.log(error));
+      if (index % 2) {
+        await user1.addFriend(user2);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default seed;
